9fdfafe46b40dbac3c9d41cfed871a1ade11acfc
Alec-FW <391491+Alec-FW@users.noreply.github.com>
2026-09-08 22:55:27 +0300

    update

--- src/win32_flickcurl_config.h
+++ src/win32_flickcurl_config.h
@@ -28,13 +28,20 @@
 extern "C" {
 #endif
 
+/* For struct timeval */
+#include <winsock2.h>
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <io.h>
 #include <memory.h>
+#include <fcntl.h>
+#include <bcrypt.h>
+#include <errno.h>
 
-
-/* MS names for these functions */
+/* MS names for these functions (vsnprintf only needed on old MSVC < 2015) */
+#if _MSC_VER < 1900
 #define vsnprintf _vsnprintf
+#endif
 #define access _access
 
 /* define missing flag for access() - the only one used here */
@@ -42,10 +49,34 @@ extern "C" {
 #define R_OK 4
 #endif
 
+/* Implemented in win32_extras.c */
+
+/* getentropy compatibility for Windows - wraps BCryptGenRandom */
+#ifndef HAVE_GETENTROPY
+int getentropy(void *buf, size_t nbytes);
+/* Can't define HAVE_GETENTROPY since this leads to source files including nonexistent <sys/random.h>,
+ * have to rely on defined(HAVE_GETENTROPY) || defined(WIN32) instead */
+/* #define HAVE_GETENTROPY 1 */
+#endif
+
+#ifndef HAVE_GETTIMEOFDAY
+int gettimeofday(struct timeval* tv, void* tz);
+#define HAVE_GETTIMEOFDAY 1
+#endif
+
+#ifndef HAVE_MKSTEMP
+int mkstemp(char* template);
+#define HAVE_MKSTEMP 1
+#endif
+
 
 /* 
  * All defines from config.h should be added here with appropriate values
  */
+
+/* vcpkg libcurl (>= 7.56) has the modern mime API
+ * (configure would find it via AC_CHECK_FUNC(curl_mime_init)) */
+#define HAVE_LIBCURL_CURL_MIME_INIT 1
 
 #undef HAVE_NANOSLEEP
 
@@ -53,6 +84,24 @@ extern "C" {
  * handles it
  */
 #define HAVE_VSNPRINTF 1
+
+/* UCRT (VS2015+) vsnprintf is C99 compliant */
+#if _MSC_VER >= 1900
+#define HAVE_C99_VSNPRINTF 1
+#endif
+
+/* use our own getopt */
+#undef HAVE_GETOPT
+#undef HAVE_GETOPT_LONG
+
+/* Raptor RDF */
+#undef HAVE_RAPTOR
+
+/* Platform features */
+#define HAVE_ERRNO_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_TIME_H 1
 
 /* FIXME - rest of win32 config defines should go here */
 
